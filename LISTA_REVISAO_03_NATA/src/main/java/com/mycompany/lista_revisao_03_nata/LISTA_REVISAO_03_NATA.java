e0b23c3d8ca733944ea8943db1415fe96a710a90
Natã Jordão <141784916+333NNN@users.noreply.github.com>
2024-09-25 21:42:27 -0300

    Add files via upload

---
+++ LISTA_REVISAO_03_NATA/src/main/java/com/mycompany/lista_revisao_03_nata/LISTA_REVISAO_03_NATA.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lista_revisao_03_nata;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ncunha
+ */
+public class LISTA_REVISAO_03_NATA {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        int n1, n2, result;
+
+        System.out.println("Insira um número: ");
+        n1 = ler.nextInt();
+        System.out.println("Insira outro número: ");
+        n2 = ler.nextInt();
+        result = n1 + n2;
+        
+        if(n1>0 && n2<0 || n1<0 && n2>0){
+            System.out.println("Dados de Entrada são Inválidos");
+        }
+        else {
+            System.out.println("Soma de " + n1 + " com " + n2 + " = " + result);
+        }
+    }
+}
